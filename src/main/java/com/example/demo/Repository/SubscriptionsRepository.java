9343f2da151f21ae42d085c020234d4d3372da7a
HananSaeed <1900948@eng.asu.edu.eg>
2024-10-12 21:45:10 +0200

    adding Repositories

--- src/main/java/com/example/demo/Repository/SubscriptionsRepository.java
+++ src/main/java/com/example/demo/Repository/SubscriptionsRepository.java
@@ -1,9 +1,9 @@
 package com.example.demo.Repository;
 
-import com.example.demo.entites.User;
+import com.example.demo.entites.Subscriptions;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface SubscriptionsRepository extends JpaRepository<Subscriptions, Integer> {
 }
