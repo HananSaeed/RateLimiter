94e310c47593109bbb4dc61f31a582883df8dd53
HananSaeed <1900948@eng.asu.edu.eg>
2024-10-14 12:41:26 +0200

    adding new methods

--- src/main/java/com/example/demo/Redis/RedisService.java
+++ src/main/java/com/example/demo/Redis/RedisService.java
@@ -3,6 +3,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -30,4 +31,22 @@ public class RedisService {
     public boolean deleteData(String key) {
         return redisTemplate.delete(key);
     }
+
+    public void saveTimestamp(String key, long timestamp) {
+        redisTemplate.opsForZSet().add(key, timestamp, timestamp);
+    }
+
+    public Set<Object> getTimestamps(String key, long start, long end) {
+        return redisTemplate.opsForZSet().rangeByScore(key, start, end);
+    }
+
+    public void removeOldTimestamps(String key, long timestamp) {
+        redisTemplate.opsForZSet().removeRangeByScore(key, 0, timestamp);
+    }
+
+    public void setExpiration(String key, long timeout, TimeUnit unit) {
+        redisTemplate.expire(key, timeout, unit);
+    }
+
+
 }
